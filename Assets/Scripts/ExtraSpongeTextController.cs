050df643d2b2d7ceb21573d92c41f250c5f6d924
Bayoe <bayoe.otto@kairosrenatus.com>
2015-06-30 16:51:52 +1000

    forgot to push

---
+++ Assets/Scripts/ExtraSpongeTextController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExtraSpongeTextController : MonoBehaviour {
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float time = SpawnController.instance.time;
+        int minutes = Mathf.FloorToInt(time) / 60;
+        int sponges = minutes * 4;
+        text.text = sponges.ToString();
+	}
+}
